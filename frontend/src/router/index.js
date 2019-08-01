e2ae260bc71a7fde06b1a2dfbf13a8534ea1dc6c
Mintchoco91 <boy0221@hanmail.net>
2019-08-01 13:30:54 +0900

    express_vue_exam

---
+++ frontend/src/router/index.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+import Vue from 'vue'
+import Router from 'vue-router'
+import MovieListPage from '@/components/MovieListPage'
+import DetailMoviePage from '@/components/DetailMoviePage'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [{
+      path: '/',
+      name: 'movielist',
+      component: MovieListPage
+    },
+    {
+      path: '/:id',
+      name: 'detailmovie',
+      component: DetailMoviePage
+    }
+  ]
+})
+
