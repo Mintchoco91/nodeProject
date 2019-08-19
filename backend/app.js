41c11f2de4129a4e43e645046e5e7ddb5c2ecfd7
Mintchoco91 <boy0221@hanmail.net>
2019-08-20 00:30:11 +0900

    renderVue 추가

--- backend/app.js
+++ backend/app.js
@@ -38,7 +38,7 @@ app.use(cookieParser());
 
 // route
 //app.use('/movies', movieRouter);
-app.use('/board', boardRouter);
+app.use('/', boardRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
