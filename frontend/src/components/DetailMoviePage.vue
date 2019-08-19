b2fc4a6dcc3b8b6158c475c7ccdc323de293b9d1
Mintchoco91 <boy0221@hanmail.net>
2019-08-19 23:10:06 +0900

    vue_render적용

--- frontend/src/components/DetailMoviePage.vue
+++ frontend/src/components/DetailMoviePage.vue
@@ -1,33 +1,17 @@
 <template>
-  <div>
-    <h1>상세 내용</h1>
-    <div class="container">
-      <div class="outer">
-        <div class="inner">
-          <div class="centered">
-            <img v-bind:src="movie.poster" class="poster" />
-            <div>
-              <strong>{{movie.name}}</strong> -
-              <i>{{movie.director}}</i>
-              [{{movie.year}}]
-              <p>{{movie.description}}</p>
-              <router-link :to="{ name: 'movielist' }">돌아가기</router-link>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</template> <script>
-/* eslint-disable */ export default {
-  created: function() {
-    var id = this.$route.params.id;
-    this.$http.get(`/movies/${id}`).then(response => {
-      this.movie = response.data[0];
-    });
-  },
-  data: function() {
-    return { movie: {} };
-  }
-};
+<h1>{{textvals}}</h1>
+</template>
+<script>
+    module.exports = {
+        data: function () {
+            return {
+                textvals: ''
+            }
+        },
+        methods: {
+            test: function () {
+                console.log('hi');
+            }
+        }
+    }
 </script>
