0d2f1b95ba7414b0960de98f5ee873f5cdf6f31f
Mintchoco91 <boy0221@hanmail.net>
2019-10-20 01:48:51 +0900

    Heroku API

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 var path = require('path');
 
-//CORS해결해야한다.
+//API 
 router.get('/', function(req, res){
     res.json("heroku 통신성공!");
 });
