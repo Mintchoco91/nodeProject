07b86c430d5247677c9e1a61bee433d47c39bf92
Mintchoco91 <boy0221@hanmail.net>
2019-10-28 22:11:50 +0900

    heroku setting

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,7 +9,7 @@ import VueSocketIOExt from 'vue-socket.io-extended';
 import io from 'socket.io-client';
  
 //const socket = io('http://localhost:3000');
-const socket = io('http://kkwnodeproject.herokuapp.com/');
+const socket = io('https://kkwnodeproject.herokuapp.com/');
 
 
 Vue.use(VueSocketIOExt, socket);
